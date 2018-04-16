b67460f8e6894f2329af92d278729adefc3b7b75
JerryLo <s86186654418@hotmail.com>
2018-04-16 15:06:04 +0800

    add project file

---
+++ H4_SN/H4_SN/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace H4_SN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "file name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 81);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(350, 302);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(368, 81);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(350, 302);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(724, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "save as";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 470);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ H4_SN/H4_SN/Form1.cs
@@ -0,0 +1,110 @@
+﻿/*type number and select template .txt will generate specified SN*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace H4_SN
+{
+    public partial class Form1 : Form
+    {
+        String lineline;
+        String fmt = "000000.##";
+        int SN = 1;
+        int line_count;
+        int time = 0;
+        int data = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String auto_path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        String path = @"C:\Users\gan\Desktop\H4_SN";
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String number = textBox3.Text;
+            SN = int.Parse(number);
+            DirectoryInfo di = Directory.CreateDirectory(path);
+            //Console.Write("start\r\n=====\r\n");
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+                
+                //lineline = sr_txt.ReadLine();
+                for (time = 1; time <= SN; time++)
+                {
+                    StreamReader sr_txt = new StreamReader(openFileDialog1.FileName);
+                    line_count = 0;
+                    int data_temp = 0;
+
+                    while ((lineline = sr_txt.ReadLine()) != null)
+                    {
+                        //Console.WriteLine(lineline);
+                        line_count++;
+                        textBox1.Text += lineline;
+                        textBox1.Text += "\r\n";
+                        if (line_count == 75 || line_count == 77 || line_count == 79 || line_count == 81)
+                        {
+                            String addr = "";
+
+                            switch (line_count)
+                            {
+                                case 75:    //千
+                                    addr = "48"; data_temp = time / 1000;
+                                    break;
+                                case 77:    //百
+                                    addr = "4A"; data_temp = (time / 100) % 10;
+                                    break;
+                                case 79:    //十
+                                    addr = "4C"; data_temp = (time / 10) % 10;
+                                    break;
+                                case 81:    //個 
+                                    addr = "4E"; data_temp = time % 10;
+                                    break;
+                            }
+                            textBox2.Text += addr + "\t";
+                            textBox2.Text += (data_temp + 30).ToString();
+                            textBox2.Text += "\t00000000";
+                            textBox2.Text += "\r\n";
+                        }
+                        else
+                        {
+                            textBox2.Text += lineline;
+                            textBox2.Text += "\r\n";
+                        }
+
+                    }
+                    System.IO.File.WriteAllText(@"C:\Users\gan\Desktop\H4_SN\815LP282030" + time.ToString(fmt) + ".txt", textBox2.Text);
+                    textBox2.Clear();
+                }
+                Console.WriteLine("There are {0} lines.",line_count);
+                MessageBox.Show("DONE");
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String auto_path = Environment.GetFolderPath( Environment.SpecialFolder.DesktopDirectory);
+            String path = @"C:\Users\gan\Desktop\testfolder";
+            DirectoryInfo di = Directory.CreateDirectory(path);
+            for (time = 0; time < 2; time++)
+            {
+                System.IO.File.WriteAllText(@"C:\Users\gan\Desktop\testfolder\test" + time + ".txt", textBox2.Text);
+            }
+        }
+            
+    }
+}
